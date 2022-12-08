c899a5d67835642acf898156fa6035e821582bf3
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-08 17:11:50 -0300

    consertando erros do lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,5 @@
-const {species} = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+// array.hofs((elemento) => {})
 
 const getSpeciesByIds = (...ids) => {
   if (ids === undefined) {
@@ -8,6 +8,6 @@ const getSpeciesByIds = (...ids) => {
   const especie = species.filter(({ id }) => ids.includes(id));
   return especie;
 };
- 
+//  console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
