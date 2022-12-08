9225d67d8d354b5f65a8eff31d4eb8e2347a8752
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-08 17:09:28 -0300

    requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const {species} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids === undefined) {
+    return [];
+  }
+  const especie = species.filter(({ id }) => ids.includes(id));
+  return especie;
 };
+ 
 
 module.exports = getSpeciesByIds;
