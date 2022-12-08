de26c769e3f379af5720dc5a0a72324c0f5d4153
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-08 18:00:35 -0300

    atualizando

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,5 +9,6 @@ const getSpeciesByIds = (...ids) => {
   return especie;
 };
 //  console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// array1.includes(2);
 
 module.exports = getSpeciesByIds;
