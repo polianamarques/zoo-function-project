dc4fbd924046ea4f17fd47155706aaf20a299d86
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-09 17:10:12 -0300

    iniciando o teste

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,10 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testa se a função getOpeningHours é definida', () => {
+    expect(getOpeningHours).toBeDefined();
+  });
+  it('Testa se a função getOpeningHours é uma função', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+});
