09b1ae2b1a1949bfa1e783bebe68f507d1a7eb6b
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-09 19:02:10 -0300

    nova tentativa requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,15 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const person = (id) => data.employees.find((employees) => employees.id === id).responsibleFor[0];
+const animals = (responsibleId) => data.species.find((animal) => animal.id === person).residents;
+const oldest = (animal) => animals.sort((a, b) => b.age - a.age)[0];
+
 const getOldestFromFirstSpecies = (id) => {
-  const responsible = data.employees.find((employee) => employee.id === id).responsibleFor[0];
-  const animals = data.species.find((animal) => animal.id === responsible).residents;
-  
+  const $animal = person(id);
+  const $species = animals($animal);
+  const $oldest = oldest($species);
+  return Object.values($oldest);
 };
+console.log(getOldestFromFirstSpecies('78460a91-f4da-4dea-a469-86fd2b8ccc84'));
 
 module.exports = getOldestFromFirstSpecies;
