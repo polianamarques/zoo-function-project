62beeb268fba99ab90cbfb998d45f1c7a5c32ece
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-13 14:43:03 -0300

    iniciando requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -7,4 +7,15 @@ describe('Testes da função getOpeningHours', () => {
   it('Testa se a função getOpeningHours é uma função', () => {
     expect(typeof getOpeningHours).toBe('function');
   });
+  it('Testa se a  função sem argumentos retorna um objeto', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
 });
