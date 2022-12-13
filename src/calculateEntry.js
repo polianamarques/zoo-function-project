046c57d1beff685f1aac5aaeb8554e3872cee5de
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-13 16:49:10 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const childs = entrants.filter((entrant) => entrant.age < 18).length;
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const seniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child: childs,
+    adult: adults,
+    senior: seniors,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || !entrants.length) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const value = prices.child * child + prices.adult * adult + prices.senior * senior;
+  return value;
 };
-// iniciando o projeto
+console.log(calculateEntry([
+	{ name:  'Lara Carvalho', age:  5 },
+	{ name:  'Frederico Moreira', age:  5 },
+	{ name:  'Pedro Henrique Carvalho', age:  5 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+]));
 module.exports = { calculateEntry, countEntrants };
