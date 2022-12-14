d5d301c85ed355c0e0478b2f7b4ceb17ea12011b
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-14 14:41:02 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,13 @@
 const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => {
-  employees.some(({ managers }) => managers.includes(id));
-};
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const manager = employees.filter(({ managers }) => managers.includes(managerId));
+  return manager.map((name) => `${name.firstName} ${name.lastName}`);
 };
+// console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
