6fef9125feb733316d36b70dec52649e6f265bb7
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-01-11 18:15:58 -0300

    iniciando requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,12 @@
 const data = require('../data/zoo_data');
 
+const daysOfTheWeek = (scheduleTarget) => {
+  const animal = data.species.find(({ name }) => name.includes(scheduleTarget));
+  const { availability } = animal;
+  return availability;
+};
+
+
 const getSchedule = (scheduleTarget) => {
   // seu código aqui
 };
